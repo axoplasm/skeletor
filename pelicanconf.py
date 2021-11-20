dde1661270eda0eb8a436671a6fb57d953d4736f
Paul Souders <paul@axoplasm.com>
2021-11-20 10:57:11 -0800

    remove feeds

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,5 +21,11 @@ PAGE_PATHS = ['']
 PAGE_SAVE_AS = '{slug}.html'
 DIRECT_TEMPLATES = []
 
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
