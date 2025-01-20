2349418df36573e1511760575921a7c646899161
Paul Souders <paul@axoplasm.com>
2025-01-20 15:04:54 -0800

    IBM Plex family; Sans in many weights

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,6 +4,15 @@ module.exports = function(eleventyConfig) {
         port: 8000
     });
     eleventyConfig.addPassthroughCopy('static');
+    eleventyConfig.addPassthroughCopy(
+        {'node_modules/@ibm/plex/IBM-Plex-Sans/fonts/complete/woff2/*.woff2': 'static/fonts'}
+    );
+    eleventyConfig.addPassthroughCopy(
+        {'node_modules/@ibm/plex/IBM-Plex-Serif/fonts/complete/woff2/*.woff2': 'static/fonts'}
+    );
+    eleventyConfig.addPassthroughCopy(
+        {'node_modules/@ibm/plex/IBM-Plex-Mono/fonts/complete/woff2/*.woff2': 'static/fonts'}
+    );
     return {
         dir: {
             input: "views",
